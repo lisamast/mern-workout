a54adf2e2d5f0c804d676048a0f7979a794b8305
lisamast <9027431@student.zadkine.nl>
2026-03-04 12:58:04 +0100

    opdracht 1&2

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+// server.js
+import express from 'express';
+import workoutRoutes from './src/routes/workoutRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/workouts', workoutRoutes);
+
+// Test route
+app.get('/', (req, res) => {
+  res.json({ message: 'Backend draait!' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server draait op http://localhost:${PORT}`);
+});
+
